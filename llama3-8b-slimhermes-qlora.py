3d3d9b42e2561a6ed73bac82eb85cd7379f36f51
geronimi73 <141400217+geronimi73@users.noreply.github.com>
2024-04-19 16:05:05 +0200

    clean, move params

--- llama3-8b-slimhermes-qlora.py
+++ llama3-8b-slimhermes-qlora.py
@@ -1,20 +1,19 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments, BitsAndBytesConfig, set_seed
+from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments, BitsAndBytesConfig, set_seed, TrainerCallback
 from peft import LoraConfig
 from trl import SFTTrainer, setup_chat_format, DataCollatorForCompletionOnlyLM
 from datasets import load_dataset
 import torch, uuid, wandb
 from accelerate import Accelerator
 
-accelerator = Accelerator()
-
 set_seed(42)
-
-modelpath = "../models/llama3-8b"
-
-# 3 reserved tokens -> im_start, im_end, pad
-tokenizerpath = "../models/llama3-8b-chatML-tokenizer"
-
+accelerator = Accelerator()
 run_id = f"llama3-8b-slimhermes-{str(uuid.uuid4())}"
+modelpath = "../models/llama3-8b"
+tokenizerpath = "../models/llama3-8b-chatML-tokenizer" # slightly modified tokenizer: 3 reserved tokens -> im_start, im_end, pad
+bs, ga = (1, 8)  # batch size, grad. acc. steps
+epochs = 4
+lr = 1e-5
+max_seq_length = 1800
 
 model = AutoModelForCausalLM.from_pretrained(
     modelpath,    
@@ -27,23 +26,10 @@ model = AutoModelForCausalLM.from_pretrained(
     use_cache = False,
 )
 tokenizer = AutoTokenizer.from_pretrained(tokenizerpath)
-
-# model, tokenizer = setup_chat_format(model, tokenizer)
-# if tokenizer.pad_token in [None, tokenizer.eos_token]: 
-#     tokenizer.pad_token = tokenizer.unk_token
-
-model.generation_config.bos_token_id = 128002
-model.generation_config.eos_token_id = 128003
-model.generation_config.pad_token_id = 128004
-model.config.bos_token_id = 128002
-model.config.eos_token_id = 128003
-model.config.pad_token_id = 128004
 tokenizer.chat_template = "{% for message in messages %}{{'<|im_start|>' + message['role'] + '\n' + message['content'] + '<|im_end|>' + '\n'}}{% endfor %}"
 
 dataset = load_dataset("g-ronimo/SlimHermes")
 
-bs=1
-ga=8
 steps_per_epoch=len(dataset["train"])//(accelerator.state.num_processes*bs*ga)
 
 training_arguments = TrainingArguments(
@@ -51,12 +37,13 @@ training_arguments = TrainingArguments(
     evaluation_strategy = "steps",
     label_names = ["labels"],
     per_device_train_batch_size = bs,
+    per_device_eval_batch_size = 2,
     gradient_accumulation_steps = ga,
     save_steps = steps_per_epoch,
     eval_steps = steps_per_epoch,
     logging_steps = 1, 
-    learning_rate = 1e-5,
-    num_train_epochs = 4,
+    learning_rate = lr,
+    num_train_epochs = epochs,
     lr_scheduler_type = "constant",
     optim = 'paged_adamw_8bit',
     bf16 = True,
@@ -75,9 +62,9 @@ trainer = SFTTrainer(
         response_template = "<|im_start|>assistant", 
         tokenizer = tokenizer, 
         mlm = False),
-    max_seq_length = 1_500,
+    max_seq_length = max_seq_length,
     peft_config = LoraConfig(
-        r=8, 
+        r=16, 
         lora_alpha=16, 
         target_modules = "all-linear", 
         modules_to_save = ["lm_head", "embed_tokens"]
@@ -87,12 +74,21 @@ trainer = SFTTrainer(
     dataset_num_proc = 8,
 )
 
-for strs in [
-    "<|im_start|>",
-    "<|im_end|>",
-    "<pad>",
-]:
-    print(strs, tokenizer(strs))
+class EvaluateFirstStepCallback(TrainerCallback):
+    def on_step_end(self, args, state, control, **kwargs):
+        if state.global_step == 1:
+            control.should_evaluate = True
+trainer.add_callback(EvaluateFirstStepCallback())
+
+## Check correct tokenization of samples
+# check_samples = 1
+# for i in range(check_samples):
+#     print(f"SAMPLE #{i}")
+#     input_ids, attention_mask, labels = trainer.data_collator([trainer.train_dataset[0]]).values()
+#     print(tokenizer.decode(input_ids[0]))  
+#     for token, label in zip(input_ids[0], labels[0]):
+#         print(f"{token.item()}, '{tokenizer.decode(token)}', {label.item()}")  
+#     print()
 
 if accelerator.is_main_process:
     wandb.init(
