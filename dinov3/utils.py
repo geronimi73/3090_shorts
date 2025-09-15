56d86ac9c65ac6386e63f3248db377ec6d4abaa3
geronimi73 <gerald.stampfel+geronimo@gmail.com>
2025-09-15 12:21:25 +0200

    NSFW classifier, 100% accuracy after 5e (?)

--- dinov3/utils.py
+++ dinov3/utils.py
@@ -1,5 +1,14 @@
 import torch
 from datasets import load_dataset
+from functools import partial
+
+def collate_fn(rows, processor):
+    labels = [r["label"] for r in rows]
+    images = [r["image"] for r in rows]
+    tensors_list = [processor(images=img, return_tensors="pt")["pixel_values"] for img in images]
+    tensor = torch.cat(tensors_list)
+    
+    return tensor, labels
 
 def load_fooddataset(processor, batch_size_train, batch_size_test=None):
     ds = load_dataset("johnowhitaker/imagenette2-320")
@@ -7,14 +16,6 @@ def load_fooddataset(processor, batch_size_train, batch_size_test=None):
     if batch_size_test is None:
         batch_size_test = batch_size_train // 2
     
-    def collate_fn(rows):
-        labels = [r["label"] for r in rows]
-        images = [r["image"] for r in rows]
-        tensors_list = [processor(images=img, return_tensors="pt")["pixel_values"] for img in images]
-        tensor = torch.cat(tensors_list)
-        
-        return tensor, labels
-
     dataloader_train = torch.utils.data.DataLoader(
         ds["train"], 
         batch_size = batch_size_train,
@@ -34,6 +35,56 @@ def load_fooddataset(processor, batch_size_train, batch_size_test=None):
     )
 
     return dataloader_train, dataloader_test
+
+from datasets import concatenate_datasets
+
+def load_nsfwdataset(processor, batch_size_train, batch_size_test=None, seed=42):
+    if batch_size_test is None:
+        batch_size_test = batch_size_train // 2
+
+    # Load NOT SFW
+    ds_nsfw = load_dataset("yesidobyte/nsfw1024")["train"]
+    # Load SFW = Imagenet
+    ds_sfw = load_dataset("johnowhitaker/imagenette2-320")["train"]
+
+    # Mix them equally: `mixed_dataset`
+    min_size = min(len(ds_nsfw), len(ds_sfw))
+    ds_nsfw = ds_nsfw.shuffle(seed=seed).select(range(min_size)).map(lambda x: {"label": 1})
+    
+    # Resize nsfw
+    ds_nsfw = ds_nsfw.map(
+        lambda x: {"image": x["image"].resize((320, 320))},
+        batched = False,
+        num_proc = 8,
+        desc = "Resizing NSFW images"
+    )
+    ds_sfw = ds_sfw.remove_columns("label").shuffle(seed=seed).select(range(min_size)).map(lambda x: {"label": 0})
+    mixed_dataset = concatenate_datasets([ds_nsfw, ds_sfw])
+
+    # Shuffle and split into train and test: `ds`
+    ds = mixed_dataset.train_test_split(test_size=0.1, seed=seed)
+
+    # Prepare dataloaders
+    dataloader_train = torch.utils.data.DataLoader(
+        ds["train"], 
+        batch_size = batch_size_train,
+        shuffle = True,
+        collate_fn = partial(collate_fn, processor=processor),
+        prefetch_factor = 2,
+        num_workers = 4,
+    )
+
+    dataloader_test = torch.utils.data.DataLoader(
+        ds["test"], 
+        batch_size = batch_size_test,
+        # shuffle = True,
+        collate_fn = partial(collate_fn, processor=processor),
+        prefetch_factor = 2,
+        num_workers = 4,
+    )
+
+    return dataloader_train, dataloader_test
+
 
 from tqdm import tqdm
 
